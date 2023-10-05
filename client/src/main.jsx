cf070fb83850ac6b0274d43dca7acb721c285c0b
Quincy Maynor <quincymaynor@gmail.com>
2023-10-05 16:33:05 -0500

    initial file structure, authentication, and react

---
+++ client/src/main.jsx
@@ -0,0 +1,38 @@
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom/dist'
+import './index.css'
+
+import App from './App.jsx'
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    error: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, {
+        path: '/login',
+        element: <Login />
+      }, {
+        path: '/signup',
+        element: <Signup />
+      },
+      {
+        path: '/me',
+        element: <Profile />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,46 @@
+import { Navigate, useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
+
+import Auth from '../utils/auth';
+
+const Profile = () => {
+  const { username: userParam } = useParams();
+
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+    variables: { username: userParam },
+  });
+
+  const user = data?.me || data?.user || {};
+  if (
+    Auth.loggedIn() && 
+    Auth.getProfile().authenticatedPerson.username === userParam
+  ) {
+    return <Navigate to="/me" />;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user?.username) {
+    return (
+      <h4>
+        Sign up or log in to see this!
+      </h4>
+    );
+  }
+
+  return (
+    <div>
+      <div className="">
+        <h2 className="">
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
