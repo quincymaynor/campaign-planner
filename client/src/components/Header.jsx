7b59768ff4c1a920a339c2a0f9cd9a73b351b6c9
Nichole <nicholewilburn@gmail.com>
2023-10-10 20:24:12 -0500

    logo and header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,50 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import AuthService from '../utils/auth';
+import logo from '../assets/images/logo.png';
 
 const Header = () => {
-  // Check if the user is logged in
   const isLoggedIn = AuthService.loggedIn();
 
   const logout = (event) => {
     event.preventDefault();
-    AuthService.logout(); // Use AuthService for logout
+    AuthService.logout();
   };
 
-  return(
-  <header>
-  <div className="container">
-    <div className="header-content">
-      <Link to="/">
-        <h1>Campaign Planner</h1>
-      </Link>
-      <p>All of your campaign notes in one place.</p>
-      <nav>
-        <ul>
-          {isLoggedIn ? (
-            <>
-              <li>
-                <Link to="/profile">{AuthService.getProfile().authenticatedPerson.username}'s Profile</Link>
-              </li>
-              <li>
-                <button onClick={logout}>Logout</button>
-              </li>
-            </>
-          ) : (
-            <>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-              <li>
-                <Link to="/signup">Signup</Link>
-              </li>
-            </>
-          )}
-        </ul>
-      </nav>
-    </div>
-  </div>
-</header>
+  return (
+    <header>
+      <div className="container">
+        <div className="header-content">
+          <Link to="/" className="logo-container">
+            <img src={logo} alt="Campaign Planner Logo" />
+          </Link>
+          <div className="header-buttons">
+            {isLoggedIn ? (
+              <>
+                <Link to="/profile" className="header-button">
+                  {AuthService.getProfile().authenticatedPerson.username}'s Profile
+                </Link>
+                <button className="header-button" onClick={logout}>
+                  Logout
+                </button>
+              </>
+            ) : (
+              <>
+                <Link to="/login" className="header-button">
+                  Login
+                </Link>
+                <Link to="/signup" className="header-button">
+                  Signup
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
 
--- server/server.js
+++ server/server.js
@@ -1,48 +1,47 @@
 const express = require('express');
-const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
+const { ApolloServer } = require('apollo-server-express');
+const { expressMiddleware } = require('@apollo/server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
+const cors = require('cors');
 
-require('dotenv').config(); //Add dotenv requirement
+require('dotenv').config();
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Enable CORS middleware
+app.use(cors());
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   introspection: true, // Enable introspection
 });
 
-// new instance of an Apollo server with the GraphQL schema
-const startApolloServer = async () => {
-  await server.start();
+server.applyMiddleware({
+  app,
+  path: '/graphql',
+  cors: false, // Important to set this to false
+});
 
-  app.use(express.urlencoded({ extended: false }));
-  app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-  app.use('/graphql', expressMiddleware(server, {
-    context: authMiddleware
-  }));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
-
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
-  }
-
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-    });
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
-};
+}
 
-// async function to start the server
-startApolloServer();
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  });
+});
