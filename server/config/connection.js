46e3276e69279a38daa97429c74f29a2c8e1fc4a
Quincy Maynor <quincymaynor@gmail.com>
2023-10-10 19:36:10 -0500

    fixed login

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,15 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/campaign-planner', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/campaign-planner');
 
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB database');
-});
-
-module.exports = db;
+module.exports = mongoose.connection
