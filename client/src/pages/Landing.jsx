0a44ddf07b7b08afa8a0689d479e13f538c324ce
mh <maxhalverson@live.com>
2023-10-10 21:20:39 -0500

    Created Landing.jsx, added landing page to main.jsx

---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import AuthService from '../utils/auth'; // Import your AuthService
+
+const Landing = () => {
+ 
+  return (
+    <main>
+      <div className="flex-row justify-center">
+        <div className="col-12 col-md-10 mb-3 p-3">
+          Landing Page
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Landing;
