23d2e8777a48be8441073440907bdf718dd22a9a
Nichole <nicholewilburn@gmail.com>
2023-10-12 19:59:17 -0500

    styling campaign cards mainly, lil footer

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -2,33 +2,37 @@ import React from 'react';
 import Campaign from '../components/Campaign';
 
 const Landing = () => {
-  return (
-    <div className="landing">
-      <div className="landing-header">
-        <h1>What story will you tell?</h1>
-        <p>Campaign Planner is your hub to create and share campaigns with your friends.</p>
-        <p>Create custom campaigns where you can let your imagination run wild!</p>
-      </div>
 
-      <div className="landing-examples">
-        <h2>Examples</h2>
-        <div className="campaigns">
-          <Campaign
-            title="Example Campaign 1"
-            description="An epic adventure awaits in a mystical land."
-          />
-          <Campaign
-            title="Example Campaign 2"
-            description="Join forces and save the kingdom from a dragon."
-          />
-          <Campaign
-            title="Example Campaign 3"
-            description="Explore a sci-fi universe and uncover its secrets."
-          />
+  const campaigns = [
+    { imageUrl: 'https://i.pinimg.com/736x/c5/0d/ea/c50deac621d9831f53f86bdc826fabb5.jpg', title: 'Campaign 1' },
+    { imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe5nowxSW9buY6RpTNR-nqAQQlz_Awyt9m4A&usqp=CAU', title: 'Campaign 2' },
+    { imageUrl: 'https://i.pinimg.com/736x/63/2c/f9/632cf9e1062ec2d2dfc5cec7eae813df.jpg', title: 'Campaign 3' }
+    // Add more campaign data as needed
+  ];
+  
+  return (
+      <div className="landing">
+        <div className="landing-header">
+          <h1>What story will you tell?</h1>
+          <p>Campaign Planner is your hub to create and share campaigns with your friends.</p>
+          <p>Create custom campaigns where you can let your imagination run wild!</p>
+        </div>
+    
+        <div className="landing-examples">
+          <h2>Examples</h2>
+          <div className="campaigns">
+            {campaigns.map((campaign, index) => (
+              <Campaign
+                key={index}
+                imageUrl={campaign.imageUrl}
+                title={campaign.title}
+                description={campaign.description}
+              />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default Landing;
