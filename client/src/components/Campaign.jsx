f47f872f94a9b5898e4becf2e35ddebe8e683d8f
Nichole <nicholewilburn@gmail.com>
2023-10-12 21:30:02 -0500

    FLIPPING CARDSgit add -Agit add -A

--- client/src/components/Campaign.jsx
+++ client/src/components/Campaign.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 
-const Campaign = ({ imageUrl, title }) => {
+const Campaign = ({ imageUrl, title, description }) => {
   const cardStyle = {
     backgroundImage: `url('${imageUrl}')`,
   };
 
   return (
-    <div className="campaign-card" style={cardStyle}>
-      <div className="campaign-title">{title}</div>
+    <div className="campaign-cards-container">
+      <div className="campaign-card">
+        <div className="campaign-card-inner">
+          <div className="campaign-card-front" style={cardStyle}>
+            <div className="campaign-title">{title}</div>
+          </div>
+          <div className="campaign-card-back" style={cardStyle}>
+            <div className="campaign-description">{description}</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
