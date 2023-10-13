566bc359e269dfc580cfe1fbc2f678ffa0b4c9e3
Nichole <nicholewilburn@gmail.com>
2023-10-12 19:23:13 -0500

    fixed profile page, can view your own and others now

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,41 +1,43 @@
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { Navigate } from 'react-router-dom'; // Import the Navigate component
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { QUERY_USER } from '../utils/queries';
 import AuthService from '../utils/auth';
 
 const Profile = () => {
   const { username: userParam } = useParams();
+console.log(userParam); // check if userParam is being extracted correctly
 
-  // Check if the user is authenticated
-  if (!AuthService.loggedIn()) {
-    // Redirect to the login page or show a message
-    return <Navigate to="/login" />;
-  }
 
-  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-    variables: { username: userParam },
-  });
+const { loading, data, error } = useQuery(QUERY_USER, {
+  variables: { username: userParam },
+  skip: !userParam, // Skip the query if userParam is not defined
+});
 
-  const user = data?.me || data?.user || {};
+  const user = data?.user || {};
+  console.log('Loading:', loading);
+  console.log('Error:', error);
+  console.log('Data:', data);
+
+    // Check if the user is authenticated
+    if (!AuthService.loggedIn()) {
+      // Redirect to the login page or show a message
+      return (
+        <h4>
+          Sign up or log in to see this!
+        </h4>
+      );
+    }
 
   if (loading) {
     return <div>Loading...</div>;
-  }
-
-  if (!user?.username) {
-    return (
-      <h4>
-        Sign up or log in to see this!
-      </h4>
-    );
   }
 
   return (
     <div>
       <div className="">
         <h2 className="">
-          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+        Viewing {userParam ? `${user?.username}'s` : 'your'} profile.
         </h2>
       </div>
     </div>
