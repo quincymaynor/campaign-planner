0967e043e1d9438be276af1413e52ad60d60ba0d
Nichole <nicholewilburn@gmail.com>
2023-10-17 20:46:42 -0500

    note page styled

--- client/src/components/NoteForm.jsx
+++ client/src/components/NoteForm.jsx
@@ -9,24 +9,29 @@ import Auth from '../utils/auth';
 const NoteForm = () => {
   const [noteTitle, setNoteTitle] = useState('');
   const [noteText, setNoteText] = useState('');
-  const [notePublic, setNotePublic] = useState(false); // Updated initial state
+  const [notePublic, setNotePublic] = useState(false);
   const [characterCount, setCharacterCount] = useState(0);
+  const [noteCampaign, setNoteCampaign] = useState(''); // Define noteCampaign state
 
-  const { loading, data } = useQuery(QUERY_ME); // Query to get the user's data
-
+  const { loading, data } = useQuery(QUERY_ME);
   const user = data?.getMe || {};
-  console.log('Loading:', loading);
-  console.log('Data:', data);
 
-  // Assuming you want to use the first campaign
-  const noteCampaign = user.campaigns && user.campaigns.length > 0 ? user.campaigns[0].id : '';
   const campaignOptions = user.campaigns && user.campaigns.length > 0
-  ? user.campaigns.map((campaign) => (
-      <option key={campaign.id} value={campaign.id}>
-        {campaign.title}
-      </option>
-    ))
-  : null;
+  ? [
+      <option key="default" value="">
+        Select Campaign
+      </option>,
+      ...user.campaigns.map((campaign) => (
+        <option key={campaign.id} value={campaign.id}>
+          {campaign.title}
+        </option>
+      ))
+    ]
+  : (
+    <option key="default" value="">
+      Select Campaign
+    </option>
+  );
 
   const [addNote, { error }] = useMutation(ADD_NOTE);
 
@@ -36,11 +41,11 @@ const NoteForm = () => {
     try {
       const { data } = await addNote({
         variables: {
-          campaignId: noteCampaign, // Assuming you have a selected campaign ID
+          campaignId: noteCampaign,
           noteTitle,
           noteText,
           notePublic,
-          noteAuthor: Auth.getProfile().authenticatedPerson.username
+          noteAuthor: Auth.getProfile().authenticatedPerson.username,
         },
       });
 
@@ -61,74 +66,74 @@ const NoteForm = () => {
     }
   };
 
+  const toggleNotePublic = () => {
+    setNotePublic(!notePublic);
+  };
+
   return (
-    <>
-    <div className="note-form-row">
-    <label className="note-form-label" htmlFor="noteCampaign">Select Campaign:</label>
-    <select
-      name="noteCampaign"
-      id="noteCampaign"
-      value={noteCampaign}
-      onChange={(e) => setNoteCampaign(e.target.value)}
-      className="form-input" // Apply the form-input class for styling
-    >
-      {campaignOptions}
-    </select>
-  </div>
-  <div className="note-form-row">
-    <label className="note-form-label" htmlFor="noteTitle">Note Title:</label>
-    <input
-      type="text"
-      name="noteTitle"
-      id="noteTitle"
-      value={noteTitle}
-      onChange={handleChange}
-      className="form-input" // Apply the form-input class for styling
-    />
-  </div>
-  <div className="note-form-row">
-    <label className="note-form-label" htmlFor="noteText">Note Text:</label>
-    <textarea
-      name="noteText"
-      id="noteText"
-      value={noteText}
-      onChange={handleChange}
-      className="form-input" // Apply the form-input class for styling
-    />
-  </div>
-  <div className="note-form-row">
-    <p className="character-count">
-      Character Count: {characterCount}/280
-    </p>
-  </div>
-  <div className="note-form-row">
-    <label className="note-form-label">Make Note Public:</label>
-    <input
-      type="checkbox"
-      name="notePublic"
-      checked={notePublic}
-      onChange={() => setNotePublic(!notePublic)}
-      className="note-form-checkbox" // Use the existing class or create a new one
-    />
-  </div>
-  <div className="note-form-row">
-    {Auth.loggedIn() ? (
-      <button onClick={handleFormSubmit} className="btn-primary">Add Note</button>
-    ) : (
-      <p>
-        You need to be logged in to write a note. Please{' '}
-        <Link to="/login" className="note-form-link">
-          login
-        </Link>{' '}
-        or{' '}
-        <Link to="/signup" className="note-form-link">
-          signup
-        </Link>
-        .
-      </p>
-    )}
-  </div>
-  </>
+    <div className="card">
+      <h4 className="card-header bg-dark text-light p-2">Create Note</h4>
+      <div className="card-body">
+        <form onSubmit={handleFormSubmit}>
+        <select
+        name="noteCampaign"
+        id="noteCampaign"
+        value={noteCampaign || ''} // Set a default empty string value
+        onChange={(e) => setNoteCampaign(e.target.value)}
+       className="form-input"
+      >
+        {campaignOptions}
+</select>
+          <input
+            type="text"
+            name="noteTitle"
+            id="noteTitle"
+            value={noteTitle}
+            onChange={handleChange}
+            className="form-input"
+            placeholder="Note Title"
+          />
+          <textarea
+            name="noteText"
+            id="noteText"
+            value={noteText}
+            onChange={handleChange}
+            className="form-input"
+            placeholder="Note Text"
+          />
+          <div className="character-count">Character Count: {characterCount}/280</div>
+          <div className="note-public-checkbox">
+            <label className="form-label">Make Note Public:</label>
+            <input
+              type="checkbox"
+              name="notePublic"
+              checked={notePublic}
+              onChange={toggleNotePublic}
+            />
+          </div>
+          {Auth.loggedIn() ? (
+            <button onClick={handleFormSubmit} className="btn btn-block btn-primary">
+              Add Note
+            </button>
+          ) : (
+            <p>
+              You need to be logged in to write a note. Please{' '}
+              <Link to="/login" className="note-form-link">
+                login
+              </Link>{' '}
+              or{' '}
+              <Link to="/signup" className="note-form-link">
+                signup
+              </Link>
+              .
+            </p>
+          )}
+        </form>
+        {error && (
+          <div className="my-3 p-3 bg-danger text-white">{error.message}</div>
+        )}
+      </div>
+    </div>
   );
 };
 
