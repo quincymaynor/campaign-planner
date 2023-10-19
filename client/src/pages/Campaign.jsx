a9973dd26f972cb3a2a5a199435f52da76600238
Nichole <nicholewilburn@gmail.com>
2023-10-19 10:00:07 -0500

    campaign page done and some other small fixes to styling

--- client/src/pages/Campaign.jsx
+++ client/src/pages/Campaign.jsx
@@ -30,47 +30,64 @@ const Campaign = () => {
             </h4>
         );
     }
+    
+    console.log(`${campaign?.campaignImage}`);
+    const imgStyle = {
+        backgroundImage: `url('../Campaign-Images/${campaign?.campaignImage}')`,
+      };
 
     return (
         <main>
-            <div className="dashboard">
-                <Tools />
-                <div className="dashboard-container">
-                    <div className="text-center mt-3">
-                        <h1>{campaign?.campaignTitle}</h1>
-                        {campaignCreatedOn ? (
-                            <small>Campaign created on: {campaignCreatedOn}</small>
-                        ) : (
-                            <small>No Campaign Creation Date</small>
-                        )}
-                        <br />
-                        {campaignDescription ? (
-                            <p>{campaignDescription}</p>
-                        ) : (
-                            <p>There is no campaign description yet!</p>
-                        )}
-                    </div>
-                    <div className="flex-row justify-center">
-                        <div className="col-12 col-md-10 mb-3 p-3">
-                            <div>
-                                <div className="campaign-card-container">
-                                    <div>
-                                        <NoteList
-                                            notes={privateNotes}
-                                            title="Private Notes"
-                                        />
-                                        <NoteList
-                                            notes={publicNotes}
-                                            title="Public Notes"
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+        <div className="dashboard">
+          <Tools />
+          <div className="dashboard-container">
+            <div className="text-center mt-3">
+              <div className="campaign-details">
+                <div className="campaign-details-left">
+                  <h1 className="no-margin-bottom">{campaign?.campaignTitle}</h1>
+                  {campaignCreatedOn ? (
+                    <small className="campaign-created no-margin-top">
+                      Campaign created on: {campaignCreatedOn}
+                    </small>
+                  ) : (
+                    <small className="campaign-created no-margin-top">No Campaign Creation Date</small>
+                  )}
+                  {campaignDescription ? (
+                    <p>{campaignDescription}</p>
+                  ) : (
+                    <p>There is no campaign description yet!</p>
+                  )}
                 </div>
+                <div className="campaign-details-right">
+                  <img src={`../Campaign-Images/${campaign?.campaignImage}`} alt="Campaign" className="campaign-image" />
+                </div>
+              </div>
+              <div className="flex-row justify-center">
+                <div className="col-12 col-md-10 mb-3 p-3">
+                  <div>
+                    <div className="campaign-card-container">
+                      <div>
+                        <NoteList
+                          notes={privateNotes}
+                          title="Private Notes"
+                        />
+                        <NoteList
+                          notes={publicNotes}
+                          title="Public Notes"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="campaign-buttons">
+                    <button className="edit-button">Edit Campaign</button>
+                    <button className="delete-button">Delete Campaign</button>
+                  </div>
+                </div>
+              </div>
             </div>
-        </main>
+          </div>
+        </div>
+      </main>
     );
 };
 
